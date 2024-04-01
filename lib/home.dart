9e45d4423805162dd1d492db224fe88ac12532f1
Nwet Mon Aye <nwetmonaye@bimgoc.com>
2024-04-02 00:00:41 +0630

    'nma'

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Push Notification"),
+      ),
+      body: const Center(
+        child: Text(
+          "Let's learn to push notification in Flutter",
+        ),
+      ),
+    );
+  }
+}
