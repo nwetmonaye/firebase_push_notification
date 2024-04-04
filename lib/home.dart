c1e048cf41b9801779c69e75216e7e9b99e3be19
nwetmonaye <nwetmon7@gmail.com>
2024-04-05 01:33:44 +0630

    CRUD with cloud firestore

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pushnotification/services/firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,17 +12,99 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //fiestore
+  final FirestoreService firestoreService = FirestoreService();
+
+  final TextEditingController textController = TextEditingController();
+  //open a dialog box to add a note
+  void openNoteBox({String? docID}) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              //add a new note
+              if (docID == null) {
+                firestoreService.addNote(textController.text);
+              }
+              // update a new note
+              else {
+                firestoreService.updateNote(docID, textController.text);
+              }
+              //clear text controller
+              textController.clear();
+
+              Navigator.pop(context);
+            },
+            child: const Text("Add"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Push Notification"),
       ),
-      body: const Center(
-        child: Text(
-          "Let's learn to Push Notification in Flutter",
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: openNoteBox,
+        child: const Icon(Icons.add),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: firestoreService.getNotesStream(),
+          builder: (context, snapshot) {
+            //if we have data , get all the docs
+            if (snapshot.hasData) {
+              List notesList = snapshot.data!.docs;
+              // display as a list
+              return ListView.builder(
+                  itemCount: notesList.length,
+                  itemBuilder: (context, index) {
+                    // get each individual doc
+                    DocumentSnapshot document = notesList[index];
+                    String docID = document.id;
+                    // get note from each doc
+                    Map<String, dynamic> data =
+                        document.data() as Map<String, dynamic>;
+
+                    String noteText = data['note'];
+
+                    // display as a list tile
+
+                    return Card(
+                      child: ListTile(
+                          title: Text(noteText),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                onPressed: () => openNoteBox(docID: docID),
+                                icon: const Icon(
+                                  Icons.settings,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () =>
+                                    firestoreService.deleteNote(docID),
+                                icon: const Icon(
+                                  Icons.delete,
+                                ),
+                              ),
+                            ],
+                          )),
+                    );
+                  });
+            } else {
+              return const Text("No notes... ");
+            }
+          }),
     );
   }
 }
